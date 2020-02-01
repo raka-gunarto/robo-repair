d01c12262fe0bc8f9deb1ce7d9616066257bf984
Joss Bird <admin@plasam.xyz>
2020-02-01 11:36:23 +0000

    Initial GUI Commit

---
+++ Assets/RotateBoard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBoard : MonoBehaviour
+{
+
+    public float rotationRate = 180;   
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationRate * Time.deltaTime, 0);
+    }
+}
