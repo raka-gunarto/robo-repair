28b65d6b2cb27e2055df3e08a83943834e9ecdd2
Joss Bird <admin@plasam.xyz>
2020-02-01 13:12:57 +0000

    Much GUI Very WOW

---
+++ Assets/Scripts/CanvasGroupManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasGroupManager : MonoBehaviour
+{
+
+    private CanvasGroup group;
+
+    private void Start()
+    {
+        group = GetComponent<CanvasGroup>();
+    }
+
+    public void SetVisibility(bool visible)
+    {
+        group.alpha = visible ? 1f : 0f;
+        group.interactable = visible;
+    }
+}
