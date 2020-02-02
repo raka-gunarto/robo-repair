fbe0ce42efee27c56557de16888d63ac7cd61cf9
Raka Gunarto <rakagunarto@gmail.com>
2020-02-02 15:10:14 +0000

    Main Menu

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneName: "Final_Map");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
